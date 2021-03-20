c4fc35fd165ae4aecc79562ce725315a387fc9cd
Santiago Vasquez <sanvara2006@gmail.com>
2021-03-20 15:42:26 -0500

    fix(code): More validations and status corrections

--- src/database/models/Contact.js
+++ src/database/models/Contact.js
@@ -5,11 +5,26 @@ const ttl = require('mongoose-ttl');
 const { Schema, model } = mongoose;
 
 const contactSchema = new Schema({
-  ip: String,
-  fullName: String,
-  affair: String,
-  email: String,
-  message: String,
+  ip: {
+    type: String,
+    required: true,
+  },
+  fullName: {
+    type: String,
+    required: true,
+  },
+  affair: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
 });
 
 contactSchema.plugin(ttl, { ttl: 604800000 });
